5e705375b39bcc042c18897d1e460cf297c1806b
Dell <druxik@gmail.com>
2018-11-23 16:23:31 +0200

    first

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body" @click="allo">
+                        I'm an example component.{{test}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        data : function () {
+            return {
+                test : "Y"
+            }
+        },
+        methods : {
+            allo : function () {
+                alert()
+            }
+        }
+    }
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <script src="{{asset("js/app.js")}}"></script>
+        <link rel="stylesheet" href="{{asset("css/app.css")}}">
+        <!-- Styles -->
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div id="app">
+                <example-component></example-component>
+            </div>
+
+        </div>
+    </body>
+</html>
